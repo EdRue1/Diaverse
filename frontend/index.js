84c8ffdadab5def35839472c9f7815c2de7c5a43
Eduardo Rueda <efrueda-pliego@uh.edu>
2025-04-05 19:22:45 -0500

    Working frontend, still needs to be fixed to look better

--- frontend/index.js
+++ frontend/index.js
@@ -23,6 +23,10 @@ app.use(bodyParser.json());
 app.get('/',function (req, res) {
     res.render('pages/home')
 });
+//home page
+app.get('/home',function (req, res) {
+    res.render('pages/home')
+});
 
 // a test to check applicants
 app.get('/allapplicants',function (req, res) {
@@ -39,27 +43,100 @@ app.get('/allapplicants',function (req, res) {
     });
 })
 
-// add applicant
+// ADD APPLICANT
+// First display the page
+app.get('/applicantform', function (req, res) {
+    res.render('pages/applicantform');
+});
+
+// Then send the information
 app.post('/applicantform', function (req, res) {
-    var message5 = req.body.fnamtoadd;
-    var message2 = req.body.lnamtoadd;
-    var message7 = req.body.chage;
-    var message6 = req.body.clrmtoadd;
-    var initial_api = 'http://127.0.0.1:5000/api/child'
+    var message1 = req.body.AFirstName;
+    var message2 = req.body.ALastName;
+    var message3 = req.body.DOB;
+    var message4 = req.body.Address;
+    var message5 = req.body.ACity;
+    var message6 = req.body.AState;
+    var message7 = req.body.ZipCode;
+    var message8 = req.body.Email;
+    var message9 = req.body.APhone;
+    var message10 = req.body.SSN;
+    var message11 = req.body.USCitizen;
+    var message12 = req.body.EligWork;
+    var message13 = req.body.CertifyTrue;
+    var message14 = req.body.Authorize;
+    var message15 = req.body.NoFalseInfo;
+    var message16 = req.body.DigitalSign;
+    var message17 = req.body.SchoolName;
+    var message18 = req.body.ECity;
+    var message19 = req.body.EState;
+    var message20 = req.body.Degree;
+    var message21 = req.body.Major;
+    var message22 = req.body.EStartYear;
+    var message23 = req.body.EEndYear;
+    var message24 = req.body.OtherCerts;
+    var message25 = req.body.PCompanyName;
+    var message26 = req.body.PCity;
+    var message27 = req.body.PState;
+    var message28 = req.body.Position;
+    var message29 = req.body.Duties;
+    var message30 = req.body.PStartYear;
+    var message31 = req.body.PEndYear;
+    var message32 = req.body.Supervisor;
+    var message33 = req.body.ReaForLeav;
+    var message34 = req.body.MayContact;
+    var message35 = req.body.RFirstName;
+    var message36 = req.body.RLastName;
+    var message37 = req.body.Title;
+    var message38 = req.body.RCompanyName;
+    var message39 = req.body.RPhone;
+
+    var initial_api = 'http://127.0.0.1:5000/add'
 
     axios.post(initial_api, {
-        firstname: message5, 
-        lastname: message2, 
-        age: message7, 
-        room: message6
+        AFirstName: message1, 
+        ALastName: message2, 
+        DOB: message3, 
+        Address: message4,
+        ACity: message5,
+        AState: message6,
+        ZipCode: message7,
+        Email: message8,
+        APhone: message9,
+        SSN: message10,
+        USCitizen: message11,
+        EligWork: message12,
+        CertifyTrue: message13,
+        Authorize: message14, 
+        NoFalseInfo: message15,
+        DigitalSign: message16,
+        SchoolName: message17,
+        ECity: message18,
+        EState: message19,
+        Degree: message20,
+        Major: message21,
+        EStartYear: message22,
+        EEndYear: message23,
+        OtherCerts: message24,
+        PCompanyName: message25,
+        PCity: message26,
+        PState: message27,
+        Position: message28,
+        Duties: message29,
+        PStartYear: message30,
+        PEndYear: message31,
+        Supervisor: message32,
+        ReaForLeav: message33,
+        MayContact: message34,
+        RFirstName: message35,
+        RLastName: message36,
+        Title: message37,
+        RCompanyName: message38,
+        RPhone: message39
     })
     .then((response) => {
-        let a_item = response.data;
-        console.log(a_item);
-        res.render('pages/child', {
-            child: ch_items, 
-            a_child: a_item
-        });
+        console.log(response.data);
+        res.render('pages/success');
     })
 
 });
